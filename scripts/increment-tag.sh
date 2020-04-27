0dfa1849282fa4a52932d985406e3e2bc74c276e
raja.kolluru@tesco.com <raja.kolluru@tesco.com>
2020-04-27 22:06:43 +0530

    fixed incrementation

--- scripts/increment-tag.sh
+++ scripts/increment-tag.sh
@@ -6,8 +6,8 @@ git describe | cut -d- -f1  |
     { IFS="." read major minor patch
 
     case $opt in
-    major) major=$(( major + 1 ));;
-    minor) minor=$(( minor + 1 ));;
+    major) major=$(( major + 1 ));minor=1;patch=1;;
+    minor) minor=$(( minor + 1 ));patch=1;;
     patch) patch=$(( patch + 1 ));;
     *) echo "Usage: $0 'major|minor|patch'"; exit 1;;
     esac
