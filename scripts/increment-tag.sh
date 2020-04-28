140e031ba8db82b3237e697c720621a61e1a891d
raja.kolluru@tesco.com <raja.kolluru@tesco.com>
2020-04-28 10:59:29 +0530

    patched the increment

--- scripts/increment-tag.sh
+++ scripts/increment-tag.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env bash
 
 opt=$1
-
 IFS=. read major minor patch <<<"$(git describe | cut -d- -f1)"
 
-
-case $opt in
+case ${opt} in
 major) major=$(( major + 1 ));minor=1;patch=1;;
 minor) minor=$(( minor + 1 ));patch=1;;
 patch) patch=$(( patch + 1 ));;
 *) echo "Usage: $0 'major|minor|patch'"; exit 1;;
 esac
 
-ver="$major.$minor.$patch"
+ver="${major}.${minor}.${patch}"
 
 # first check that there are no pending commits.
 items_to_commit=$(git status --porcelain=1 | wc -l)
@@ -22,9 +20,9 @@ then
     exit 2
 fi
 
-git tag -a -m $ver $ver
-read -p "Successfully created a tag $ver. Want me to build the deployable?"  yes < /dev/tty
-if [[ $yes == y* ]]
+git tag -a -m ${ver} ${ver}
+read -p "Successfully created a tag $ver. Want me to build the deployable?" yes
+if [[ ${yes} == y* ]]
 then
     echo "Building the executable for you."
     gradle clean build
