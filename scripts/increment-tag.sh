5fb9271eed013399dcf3164c7fea0586c245dde3
raja.kolluru@tesco.com <raja.kolluru@tesco.com>
2020-04-28 10:55:57 +0530

    rewrote the increment version

--- scripts/increment-tag.sh
+++ scripts/increment-tag.sh
@@ -2,33 +2,33 @@
 
 opt=$1
 
-git describe | cut -d- -f1  |
-    { IFS="." read major minor patch
+IFS=. read major minor patch <<<"$(git describe | cut -d- -f1)"
 
-    case $opt in
-    major) major=$(( major + 1 ));minor=1;patch=1;;
-    minor) minor=$(( minor + 1 ));patch=1;;
-    patch) patch=$(( patch + 1 ));;
-    *) echo "Usage: $0 'major|minor|patch'"; exit 1;;
-    esac
 
-    ver="$major.$minor.$patch"
+case $opt in
+major) major=$(( major + 1 ));minor=1;patch=1;;
+minor) minor=$(( minor + 1 ));patch=1;;
+patch) patch=$(( patch + 1 ));;
+*) echo "Usage: $0 'major|minor|patch'"; exit 1;;
+esac
 
-    # first check that there are no pending commits.
-    items_to_commit=$(git status --porcelain=1 | wc -l)
-    if (( items_to_commit != 0 ))
-    then
-        echo "There are pending commit items. Please commit them before tagging."
-        exit 2
-    fi
+ver="$major.$minor.$patch"
 
-    git tag -a -m $ver $ver
-    read -p "Successfully created a tag $ver. Want me to build the deployable?"  yes
-    if [[ $yes == y* ]]
-    then
-        echo "Building the executable for you."
-        gradle clean build
-    fi
+# first check that there are no pending commits.
+items_to_commit=$(git status --porcelain=1 | wc -l)
+if (( items_to_commit != 0 ))
+then
+    echo "There are pending commit items. Please commit them before tagging."
+    exit 2
+fi
 
-    exit 0
-}
+git tag -a -m $ver $ver
+read -p "Successfully created a tag $ver. Want me to build the deployable?"  yes < /dev/tty
+if [[ $yes == y* ]]
+then
+    echo "Building the executable for you."
+    gradle clean build
+fi
+
+exit 0
+
